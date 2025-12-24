ea8976e55f17e7293d61f8f80b1c30f749c19321
Fardeen Bablu <fardeeneb@gmail.com>
2025-12-23 22:29:53 -0500

    hosting for freshr

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('dynamap-app');
+  protected readonly title = signal('Freshr');
 }
